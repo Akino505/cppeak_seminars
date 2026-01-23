634d60be3ca59519b75416aafa98fd31139646f0
Akino <9mable@gmail.com>
2026-01-23 03:02:18 +0300

    Added project_1 folder

--- build/CMakeFiles/Project_1.dir/src/mult.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/Project_1.dir/src/mult.cpp.o: \
- /home/akino/c++/project_1/src/mult.cpp /usr/include/stdc-predef.h \
- /home/akino/c++/project_1/include/mult.h
--- build/CMakeFiles/tests.dir/src/mult.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/tests.dir/src/mult.cpp.o: \
- /home/akino/c++/project_1/src/mult.cpp /usr/include/stdc-predef.h \
- /home/akino/c++/project_1/include/mult.h
