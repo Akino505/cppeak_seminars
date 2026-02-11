5566b67c7edaf8f88fba980d763b347b94bc0e33
Shilenko Aleksandr <arshilenko@edu.hse.ru>
2026-02-11 15:12:54 -0500

    first try

---
+++ project_3/build/CMakeFiles/matrixOperations.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/matrixOperations.dir/src/appropriateSize.cpp.o"
+  "CMakeFiles/matrixOperations.dir/src/appropriateSize.cpp.o.d"
+  "CMakeFiles/matrixOperations.dir/src/inputSample.cpp.o"
+  "CMakeFiles/matrixOperations.dir/src/inputSample.cpp.o.d"
+  "CMakeFiles/matrixOperations.dir/src/main.cpp.o"
+  "CMakeFiles/matrixOperations.dir/src/main.cpp.o.d"
+  "CMakeFiles/matrixOperations.dir/src/meanDeviation.cpp.o"
+  "CMakeFiles/matrixOperations.dir/src/meanDeviation.cpp.o.d"
+  "CMakeFiles/matrixOperations.dir/src/median.cpp.o"
+  "CMakeFiles/matrixOperations.dir/src/median.cpp.o.d"
+  "CMakeFiles/matrixOperations.dir/src/mode.cpp.o"
+  "CMakeFiles/matrixOperations.dir/src/mode.cpp.o.d"
+  "CMakeFiles/matrixOperations.dir/src/safeInput.cpp.o"
+  "CMakeFiles/matrixOperations.dir/src/safeInput.cpp.o.d"
+  "libmatrixOperations.a"
+  "libmatrixOperations.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/matrixOperations.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
