b4659e18b6735e5b0dab9414b3ac8202fc0c72b1
Kim Alina <alialekim@edu.hse.ru>
2026-02-12 01:11:59 +0300

    Fixed input

--- project_3/src/inputSample.cpp
+++ project_3/src/inputSample.cpp
@@ -9,11 +9,14 @@ std::vector<double> inputSample(int rows, int columns)
     double value = 0;
     std::vector<double> storage;
     std::string row;
-    std::getline(std::cin, row);
-    std::stringstream streamRow(row);
-    while(streamRow >> value)
-    {
-        storage.push_back(value);
+    while (rows){
+        std::getline(std::cin, row);
+        std::stringstream streamRow(row);
+        while(streamRow >> value)
+        {
+            storage.push_back(value);
+        }
+        rows--;
     }
     return storage;
 }
