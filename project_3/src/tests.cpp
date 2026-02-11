2c89a289f4d175c10694465d2f00cc51b804082f
Kim Alina <alialekim@edu.hse.ru>
2026-02-11 21:15:20 +0300

    added headers to tests.cpp

--- project_3/src/tests.cpp
+++ project_3/src/tests.cpp
@@ -1,5 +1,9 @@
 #include <catch2/catch_test_macros.hpp>
 #include "mode.hpp"
+#include "median.hpp"
+#include "meanDeviation.hpp"
+#include "safeInput.hpp"
+#include "appropriateSize.hpp"
 
 TEST_CASE("Testing mode function"){
     SECTION("Simple"){
