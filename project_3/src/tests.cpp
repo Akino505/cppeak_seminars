522e06862096b4bc07bb4092d2c09ffa21887a5b
Kim Alina <alialekim@edu.hse.ru>
2026-02-11 23:59:21 +0300

    Added mode tests

--- project_3/src/tests.cpp
+++ project_3/src/tests.cpp
@@ -13,11 +13,30 @@ TEST_CASE("Testing mode function"){
         REQUIRE(mode({-2}) == -2.0);
     }
 
-    SECTION("Duplicates inside")
-    {
-        REQUIRE(mode({0.0, 0.0, 0.0, 0.0}) == 0.0);
-        REQUIRE(mode({2.0, 2.0, 1.0}) == 2.0);
-        REQUIRE(mode({-12.0, 3.0, 3.0, 1.0, 1.0}) == 1.0);
+    SECTION("All elements are equal") {
+        REQUIRE(mode({5.0, 5, 5.0, 5}) == 5.0);
+        REQUIRE(mode({-1.0, -1, -1.0}) == -1.0);
+        REQUIRE(mode({3.14, 3.14, 3.14}) == 3.14);
+    }
+
+    SECTION("All elements are unique") {
+        REQUIRE(mode({1, 2.0, 3.0, -4.0, 5.0}) == 1.0);
+        REQUIRE(mode({10.0, 20, 30.0}) == 10.0);
+        REQUIRE(mode({-5.0, 0.0, 5.0}) == -5.0);
+    }
+
+    SECTION("Single mode") {
+        REQUIRE(mode({1.0, 2.0, 2.0, 3.0, 4.0}) == 2.0);
+        REQUIRE(mode({10.0, -20.0, 30.0, 30.0, 30.0, 40}) == 30.0);
+        REQUIRE(mode({1.5, 1.5, 1.5, 2.5}) == 1.5);
+        REQUIRE(mode({-5.0, -5.0, -5.0, -10.0}) == -5.0);
+    }
+
+    SECTION("More than one mode") {
+        REQUIRE(mode({1.0, 1.0, 2.0, 2, 3.0}) == 1.0);
+        REQUIRE(mode({5.0, 5, 7.0, 7.0, 9.0, 9.0}) == 5.0);
+        REQUIRE(mode({1.0, 1, 1.0, 2.0, 2.0, 2.0}) == 1.0);
+        REQUIRE(mode({-1.0, -1.0, -2.0, -2.0}) == -1.0);
     }
 }
 
@@ -25,30 +44,30 @@ TEST_CASE("Testing median function")
 {
     SECTION("One element")
     {
-        REQUIRE(median(3.14) == (3.14, NULL));
-        REQUIRE(median(1) == (1, NULL));
-        REQUIRE(median(-2) == (-2, NULL));
+        REQUIRE(median({3.14}) == (3.14, NULL));
+        REQUIRE(median({1}) == (1, NULL));
+        REQUIRE(median({-2}) == (-2, NULL));
     }
 
     SECTION("Even amount")
     {
-        REQUIRE(median(3.14, -2.13, 4.0, 2) == (3.14, 2.0));
-        REQUIRE(median(1, 2, 3, 4) == (2.0, 3.0));
-        REQUIRE(median(-12.0, 3.0, 0, 1) == (0.0, 1.0));
+        REQUIRE(median({3.14, -2.13, 4.0, 2}) == (3.14, 2.0));
+        REQUIRE(median({1, 2, 3, 4}) == (2.0, 3.0));
+        REQUIRE(median({-12.0, 3.0, 0, 1}) == (0.0, 1.0));
     }
 
     SECTION("Odd amount")
     {
-        REQUIRE(median(3.14, -2.13, 4.0) == (3.14, NULL));
-        REQUIRE(median(1, 2, 3) == (2.0, NULL));
-        REQUIRE(median(-12.0, 3.0, 0) == (0.0, NULL));
+        REQUIRE(median({3.14, -2.13, 4.0}) == (3.14, NULL));
+        REQUIRE(median({1, 2, 3}) == (2.0, NULL));
+        REQUIRE(median({-12.0, 3.0, 0}) == (0.0, NULL));
     }
 
     SECTION("Duplicates inside")
     {
-        REQUIRE(median(0, 0, 0, 0) == (0.0, 0.0));
-        REQUIRE(median(2, 2, 1) == (2.0, NULL));
-        REQUIRE(median(-12.0, 3.0, 3.0, 1) == (1.0, 3.0));
+        REQUIRE(median({0, 0, 0, 0}) == (0.0, 0.0));
+        REQUIRE(median({2, 2, 1}) == (2.0, NULL));
+        REQUIRE(median({-12.0, 3.0, 3.0, 1}) == (1.0, 3.0));
     }
 }
 
