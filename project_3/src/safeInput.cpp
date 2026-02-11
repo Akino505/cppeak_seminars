0a4dbaf1b33b184ecbcb5cc8ae12d48127ecb1f2
Kim Alina <alialekim@edu.hse.ru>
2026-02-12 01:26:25 +0300

    Added \n

--- project_3/src/safeInput.cpp
+++ project_3/src/safeInput.cpp
@@ -12,12 +12,12 @@ bool safeInput(double value)
     bool flag = true;
     if(!isInt(value))
     {
-        std::cout << "ERROR: Size must be integer";
+        std::cout << "ERROR: Size must be integer" << "\n";
         flag = false;
     }
     if(value <= 0)
     {
-        std::cout << "ERROR: Size must be positive.";
+        std::cout << "ERROR: Size must be positive." << "\n";
         flag = false;
     }
     return flag;
