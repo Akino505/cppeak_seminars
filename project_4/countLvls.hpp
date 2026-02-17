b133811d077b2330f30b71636f5452742958c8b9
Kim Alina <alialekim@edu.hse.ru>
2026-02-17 22:15:49 +0300

    Added hpp file for counting lvls

---
+++ project_4/countLvls.hpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <array>
+#include <fstream>
+#include <string>
+#include <sstream>
+
+#ifndef COUNT_LVLS_HPP
+#define COUNT_LVLS_HPP
+
+int countLvls(std::vector<std::array<std::string, 4>> logBase, std::ofstream& out);
+
+#endif
