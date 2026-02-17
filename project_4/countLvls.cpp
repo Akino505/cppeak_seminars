512873bc8324b997953b07078f803d554102033b
Kim Alina <alialekim@edu.hse.ru>
2026-02-17 13:19:41 +0300

    counting lvls

--- project_4/countLvls.cpp
+++ project_4/countLvls.cpp
@@ -1,10 +1,22 @@
 #include <iostream>
 #include <vector>
 #include <array>
+#include <fstream>
+#include <string>
 
 int countLvls(std::vector<std::array<std::string, 4>> logBase, std::fstream& Out)
 {
-
+    int errs = 0;
+    int warns = 0;
+    int dbgs = 0;
+    int infos = 0;
+    for(int idx = 0; idx < logBase.size(); idx++)
+    {
+        if(logBase[idx][0] == "ERROR") errs++;
+        if(logBase[idx][0] == "DEBUG") dbgs++;
+        if(logBase[idx][0] == "WARNING") warns++;
+        if(logBase[idx][0] == "INFO") infos++;   
+    }
 }
 
 int main()
