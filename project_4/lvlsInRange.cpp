6d8a7a9a432a38cca80c20096b5c0dc7024c98ca
Kim Alina <alialekim@edu.hse.ru>
2026-02-18 00:54:43 +0300

    made hpp for func

--- project_4/lvlsInRange.cpp
+++ project_4/lvlsInRange.cpp
@@ -1,39 +1,7 @@
-#include <iostream>
-#include <string>
+#include "lvlsInRange.hpp"
 
-#include <fstream>
-#include <string>
-#include <sstream>
-#include <iostream>
-#include <vector>
-#include <array>
-
-std::vector<std::array<std::string, 4>> readFile(std::ifstream& file)
+bool lvlsInRange(std::vector<std::array<std::string, 4>> logBase, std::ofstream& out)
 {
-    std::string logComm = "";
-    std::string comment = "";
-    std::vector<std::array< std::string, 4>> logBase;
-    while(std::getline(file, logComm))
-    {
-        std::array<std::string, 4> logLine;
-        std::stringstream streamLog(logComm);
-        for(int idx = 0; idx < 3; idx++)
-        {
-            streamLog >> logLine[idx];
-        }
-        std::getline(streamLog, comment);
-        logLine[3] = comment;
-        logBase.push_back(logLine);
-    }
-    file.close();
-    return logBase;
-}
-
-int lvlsInRange()
-{
-    std::ifstream fileIn("log_input.txt");
-    std::ofstream fileOut("log_output.txt");
-    std::vector<std::array<std::string, 4>> logBase = readFile(fileIn);
     std::vector<std::array<std::string, 2>> timeBase = {};
     int start = 0;
     int finish = 0;
@@ -55,7 +23,7 @@ int lvlsInRange()
     if(!(std::cin >> start) || start > timeBase.size() || start < 1)
     {
         std::cout << "Please, type appropriate number." << "\n";
-        return 0;
+        return false;
     }
     std::cout << "Choose the finish time (type ONLY number):" << "\n";
     for(int idx = start - 1; idx < timeBase.size(); idx++)
@@ -65,7 +33,7 @@ int lvlsInRange()
     if(!(std::cin >> finish) || finish > timeBase.size() || finish < 1)
     {
         std::cout << "Please, type appropriate number." << "\n";
-        return 0;
+        return false;
     }
     for(int idx = start - 1; idx < finish; idx++)
     {
@@ -73,15 +41,9 @@ int lvlsInRange()
         {
             if(logBase[jdx][1] == timeBase[idx][0] && logBase[jdx][2] == timeBase[idx][1])
             {
-                fileOut << logBase[jdx][0] << " " << logBase[jdx][1] << " " << logBase[jdx][2] << " " << logBase[jdx][3] << "\n";
+                out << logBase[jdx][0] << " " << logBase[jdx][1] << " " << logBase[jdx][2] << " " << logBase[jdx][3] << "\n";
             }
         }
     }
-    return 1;
-}
-
-
-int main()
-{
-    lvlsInRange();
+    return true;
 }
---
+++ project_4/lvlsInRange.hpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <array>
+
+#ifndef LVLS_IN_RANGE
+#define LVLS_IN_RANGE
+
+bool lvlsInRange(std::vector<std::array<std::string, 4>> logBase, std::ofstream& out);
+
+#endif
