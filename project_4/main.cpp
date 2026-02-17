40e11623dab73b86adf858642f0bf3ed5bbcaeba
Kim Alina <alialekim@edu.hse.ru>
2026-02-17 22:41:32 +0300

    made draft of main

--- project_4/main.cpp
+++ project_4/main.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
-#include <fstream>
-#include <array>
-#include <vector>
-#include <string>
+#include "readFile.hpp"
+#include "countLvls.hpp"
 
 std::ostream& operator<<(std::ostream& os, const std::array<std::string, 4>& arr)
 {
@@ -12,6 +10,43 @@ std::ostream& operator<<(std::ostream& os, const std::array<std::string, 4>& arr
 
 int main()
 {
-    std::fstream file("log_input.txt");
-    std::vector<std::array<std::string, 4>> logBase = readFile(file);
+    std::ifstream fileIn("log_input.txt");
+    std::ofstream fileOut("log_output.txt");
+    std::vector<std::array<std::string, 4>> logBase = readFile(fileIn);
+    std::string command = "";
+    std::cout << "What do you want to do with the log file?" << "\n";
+    std::cout << "1. Request the output of the types of levels and their counts" << "\n";
+    std::cout << "2. Request the output of a specific level" << "\n";
+    std::cout << "3. Request the output of levels within a specific time range" << "\n";
+    std::cout << "4. Request the output of the most frequent messages for errors and warnings" << "\n";
+    std::cout << "5. Request statistics for a specific time period" << "\n";
+    std::cout << "Type 'help' for list of commands." << "\n";
+    std::cout << "Type 'exit' to exit." << "\n";
+    while(command != "exit")
+    {
+        std::cin >> command;
+        if(command == "1")
+        {   
+            countLvls(logBase, fileOut);
+            fileOut << "----------------------------------------------------------------------" << "\n";
+        }
+        else if(command == "3")
+        {
+
+        }
+        else if(command == "help")
+        {
+            std::cout << "1. Request the output of the types of levels and their counts" << "\n";
+            std::cout << "2. Request the output of a specific level" << "\n";
+            std::cout << "3. Request the output of levels within a specific time range" << "\n";
+            std::cout << "4. Request the output of the most frequent messages for errors and warnings" << "\n";
+            std::cout << "5. Request statistics for a specific time period" << "\n";
+        }
+        else
+        {
+            std::cout << "Please, type appropriate command.";
+            std::cout << "Type 'help' for list of commands." << "\n";
+        }
+    }
+    std::cout << "Goodbye! :)" << "\n";
 }
