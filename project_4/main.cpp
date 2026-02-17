3a5511ddac40c6d265f3c6468433997c54e566b1
Kim Alina <alialekim@edu.hse.ru>
2026-02-17 12:30:03 +0300

    update main

--- project_4/main.cpp
+++ project_4/main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
 #include <fstream>
+#include <array>
+
+std::ostream& operator<<(std::ostream& os, const std::array<std::string, 4>& arr)
+{
+    os << arr[0] << " " << arr[1] << " " << arr[2] << arr[3] << "\n";
+    return os;
+}
 
 int main()
 {
-    
+    std::fstream file("log_input.txt");
 }
