9ccb0a4210cfc58c98ecca550d418fdab28fcec9
Daria Kudelkina <ddkudelkina@edu.hse.ru>
2026-02-23 15:23:10 +0300

    Added files with the addTask function

---
+++ project_5/addTask.cpp
@@ -0,0 +1,27 @@
+#include "addTask.hpp"
+
+void addTask(std::unordered_map<int, std::tuple<std::string, int, bool>>& tasks, 
+             std::priority_queue<std::tuple<int, int, std::string>>& pq, 
+             int id, const std::string& desc, int prio) {
+    
+    if (prio < 0 || prio > 10) {
+        std::cerr << "Error: Priority must be in the range 0-10" << std::endl;
+        return;
+    }
+    
+    if (tasks.find(id) != tasks.end()) {
+        std::cerr << "Error: Task with ID " << id << " already exists" << std::endl;
+        return;
+    }
+    
+    if (desc.empty()) {
+        std::cerr << "Error: Description cannot be empty" << std::endl;
+        return;
+    }
+    
+    pq.push(std::make_tuple(prio, id, desc));
+
+    tasks[id] = std::make_tuple(desc, prio, false);
+    
+    std::cout << "The task has been added successfully!" << std::endl;
+}
---
+++ project_5/addTask.hpp
@@ -0,0 +1,14 @@
+#include <iostream>      
+#include <string>        
+#include <tuple>        
+#include <queue>         
+#include <unordered_map> 
+
+#ifndef ADD_TASK
+#define ADD_TASK
+
+void addTask(std::unordered_map<int, std::tuple<std::string, int, bool>>& tasks, 
+             std::priority_queue<std::tuple<int, int, std::string>>& pq, 
+             int id, const std::string& desc, int prio);
+
+#endif
