e445444ac34d35db813329f6a64552bd2ac2ce8c
Shilenko Aleksandr <arshilenko@edu.hse.ru>
2026-02-23 18:30:21 -0500

    adding undoLastTask

---
+++ project_5/undoLastTask.cpp
@@ -0,0 +1,55 @@
+#include "CompletingTask.hpp"
+
+// EXPECT: queue
+void undoLast(
+    std::stack<std::tuple<int, std::string, int>>& undoStack,
+    std::unordered_map<int, std::tuple<std::string, int, bool>>& tasks,
+    std::priority_queue<std::tuple<int, int, std::string>>& pq)
+{
+    if(!undoStack.size())
+    {
+        std::cout << "Nothing to undo!" << "\n";
+        return;
+    }
+
+    std::tuple<int, std::string, int> mjTask = undoStack.top();
+    std::tuple<int, int, std::string> TaskForUndo =
+        std::tuple(std::get<2>(mjTask),   // prioity
+                   std::get<0>(mjTask),   // ID
+                   std::get<1>(mjTask));  // info
+
+    pq.push(TaskForUndo);  // add task at the end of undoStack
+    try                    // change task's status in tasks
+    {
+        std::get<2>(tasks.at(std::get<1>(TaskForUndo))) = false;
+    }
+    catch(const std::out_of_range& e)
+    {
+        std::cout << "Task's ID not found\n";
+    }
+
+    undoStack.pop();  // pop first task from pq
+    std::cout << "Completing task:\n"
+              << "ID: " << std::get<0>(mjTask)
+              << "\nInfo: " << std::get<1>(mjTask)
+              << "\nPriority: " << std::get<2>(mjTask) << "\n";
+
+    return;
+}
+
+// int main()
+// {
+//     std::stack<std::tuple<int, std::string, int>> undoStack;
+//     std::unordered_map<int, std::tuple<std::string, int, bool>> tasks;
+//     std::priority_queue<std::tuple<int, int, std::string>> pq;
+
+//     tasks[1] = {"Go for a walk", 1, false};
+//     tasks[2] = {"Do homework in C++", 9, false};
+//     tasks[3] = {"Have a sleep", 7, false};
+
+//     undoStack.push({1, "Go for a walk", 1});
+//     undoStack.push({2, "Do homework in C++", 9});
+//     undoStack.push({3, "Have a sleep", 7});
+
+//     undoLast(undoStack, tasks, pq);
+// }
