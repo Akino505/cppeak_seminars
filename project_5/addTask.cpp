80df0943f5cb9ecad139f318f276b4a04d08aa91
Daria Kudelkina <ddkudelkina@edu.hse.ru>
2026-02-23 15:34:59 +0300

    Tests added

--- project_5/addTask.cpp
+++ project_5/addTask.cpp
@@ -1,27 +1,35 @@
 #include "addTask.hpp"
 
-void addTask(std::unordered_map<int, std::tuple<std::string, int, bool>>& tasks, 
-             std::priority_queue<std::tuple<int, int, std::string>>& pq, 
-             int id, const std::string& desc, int prio) {
-    
-    if (prio < 0 || prio > 10) {
-        std::cerr << "Error: Priority must be in the range 0-10" << std::endl;
+void addTask(
+    std::unordered_map<int, std::tuple<std::string, int, bool>>& tasks,
+    std::priority_queue<std::tuple<int, int, std::string>>& pq,
+    int id,
+    const std::string& desc,
+    int priority
+)
+{
+    if (priority < 0 || priority > 10)
+    {
+        std::cerr << "Error: Priority must be in the range 0-10"
+                  << std::endl;
         return;
     }
-    
-    if (tasks.find(id) != tasks.end()) {
-        std::cerr << "Error: Task with ID " << id << " already exists" << std::endl;
+
+    if (tasks.find(id) != tasks.end())
+    {
+        std::cerr << "Error: Task with ID " << id << " already exists"
+                  << std::endl;
         return;
     }
-    
-    if (desc.empty()) {
+
+    if (desc.empty())
+    {
         std::cerr << "Error: Description cannot be empty" << std::endl;
         return;
     }
-    
-    pq.push(std::make_tuple(prio, id, desc));
 
-    tasks[id] = std::make_tuple(desc, prio, false);
-    
+    pq.push(std::make_tuple(priority, id, desc));
+    tasks[id] = std::make_tuple(desc, priority, false);
+
     std::cout << "The task has been added successfully!" << std::endl;
 }
