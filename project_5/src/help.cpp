82dd81949f9dd7be3964ef61e0450b168bc3470d
Kim Alina <alialekim@edu.hse.ru>
2026-02-24 03:24:25 +0300

    fixed dot

--- project_5/src/help.cpp
+++ project_5/src/help.cpp
@@ -3,7 +3,7 @@
 
 void help()
 {
-    std::cout << "1. Complete the tas.k" << "\n";
+    std::cout << "1. Complete the task." << "\n";
     std::cout << "2. Cancel the task." << "\n";
     std::cout << "3. Request the list of uncompleted tasks." << "\n";
     std::cout << "Type 'help' for list of commands." << "\n";
