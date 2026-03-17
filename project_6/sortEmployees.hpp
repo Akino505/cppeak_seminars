3cc0ed8329d2ab086ac6ae4db53f03428159a4bd
Daria Kudelkina <ddkudelkina@edu.hse.ru>
2026-03-17 17:59:01 +0300

    Bugs fixed

---
+++ project_6/sortEmployees.hpp
@@ -0,0 +1,13 @@
+#ifndef SORT_EMPLOYEES_HPP
+#define SORT_EMPLOYEES_HPP
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <tuple>
+#include <functional>
+#include <cstring>
+
+void sortEmployees(SafeVector<Employee>& employees);
+
+#endif
