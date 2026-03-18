89d9e766bfe96945b45337862f1583a1a99fd6f3
Shilenko Aleksandr <arshilenko@edu.hse.ru>
2026-03-18 18:24:36 -0400

    SafeVector .hpp and .cpp added

---
+++ project_6/src/storageEmployes.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+
+#include "../include/storageEmployees.hpp"
+
+#include <iostream>
+
+SafeVector::SafeVector(): data(nullptr), size(0) {}
+SafeVector::~SafeVector() { delete[] data; }
+
+Employee& SafeVector::at(int index)
+{
+    if(index < 0 || index >= size)
+    {
+        std::cout << "Ошибка: индекс вне диапазона!" << std::endl;
+        exit(1);
+    }
+    return data[index];
+}
