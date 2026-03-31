f6c009b85f18f36ce39941db46909416adb6d0d1
Kim Alina <alialekim@edu.hse.ru>
2026-03-31 16:56:34 +0300

    added tie and greater to comparator

--- project_6/src/sortEmployees.cpp
+++ project_6/src/sortEmployees.cpp
@@ -1,12 +1,20 @@
 #include "sortEmployees.hpp"
+#include <tuple>
 
 bool compareEmployee(const Employee& first, const Employee& second)
 {
-    if (first.getDept() != second.getDept())
-        return first.getDept() < second.getDept();
-    if (first.getSalary() != second.getSalary())
-        return first.getSalary() > second.getSalary();
-    return first.getName() < second.getName();
+    int deptFirst = first.getDept();
+    double salFirst = first.getSalary();
+    std::string nameFirst = first.getName();
+    int deptSecond = second.getDept();
+    double salSecond = second.getSalary();
+    std::string nameSecond = second.getName();
+    bool cmpSalaryFirst =
+        std::greater<double>()(second.getSalary(), first.getSalary());
+    bool cmpSalarySecond =
+        std::greater<double>()(first.getSalary(), second.getSalary());
+    return std::tie(deptFirst, cmpSalaryFirst, nameFirst) <
+           std::tie(deptSecond, cmpSalarySecond, nameSecond);
 }
 
 void sortEmployees(SafeVector& employees)
