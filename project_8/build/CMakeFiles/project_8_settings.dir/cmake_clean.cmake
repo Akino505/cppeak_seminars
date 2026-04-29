b55c591abb50fffb1de95dc711d85d938c690265
Kim Alina <alialekim@edu.hse.ru>
2026-04-29 21:40:45 +0300

    Merged validation

---
+++ project_8/build/CMakeFiles/project_8_settings.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/project_8_settings.dir/src/main.cpp.o"
+  "CMakeFiles/project_8_settings.dir/src/main.cpp.o.d"
+  "project_8_settings"
+  "project_8_settings.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/project_8_settings.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
