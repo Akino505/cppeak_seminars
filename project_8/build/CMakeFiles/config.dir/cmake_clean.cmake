45cc096ca3d2827a131b72f4a4bc7c2af8f82c54
Kim Alina <alialekim@edu.hse.ru>
2026-05-19 14:31:47 +0300

    cache

---
+++ project_8/build/CMakeFiles/config.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/config.dir/src/ConfigStore.cpp.o"
+  "CMakeFiles/config.dir/src/ConfigStore.cpp.o.d"
+  "CMakeFiles/config.dir/src/ConfigValidator.cpp.o"
+  "CMakeFiles/config.dir/src/ConfigValidator.cpp.o.d"
+  "CMakeFiles/config.dir/src/main.cpp.o"
+  "CMakeFiles/config.dir/src/main.cpp.o.d"
+  "libconfig.a"
+  "libconfig.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/config.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ project_8/build/CMakeFiles/project_8.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/project_8.dir/src/main.cpp.o"
+  "CMakeFiles/project_8.dir/src/main.cpp.o.d"
+  "project_8"
+  "project_8.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/project_8.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
